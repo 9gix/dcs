16c24c6bbe2532d83957787753577fc17c803777
Eugene <yeo.eugene.oey@gmail.com>
2015-03-11 03:18:36 +0800

    Separate schema sql file

--- multimedia/migrations/0001_initial.py
+++ multimedia/migrations/0001_initial.py
@@ -3,39 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
-create_multimedia_table = """
-CREATE TABLE multimedia (
-    id INT NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    description TEXT NOT NULL,
-    price DECIMAL NULL,
-    created_at DATETIME NOT NULL,
-    modified_at DATETIME NOT NULL,
-    PRIMARY KEY (id)
-);
-"""
+with open('schema/create_multimedia.sql') as f:
+    create_multimedia_script = f.read()
 
-delete_multimedia_table = """
-DROP TABLE multimedia;
-"""
-
-create_book_table = """
-CREATE TABLE book (
-    id INT NOT NULL,
-    multimedia_id INT NOT NULL,
-    isbn13 CHAR(13) NULL,
-    isbn10 CHAR(10) NULL,
-    published_on DATE NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_book_multimedia
-        FOREIGN KEY (multimedia_id)
-        REFERENCES multimedia (id)
-);
-"""
-
-delete_book_table = """
-DROP TABLE book;
-"""
+with open('schema/drop_multimedia.sql') as f:
+    drop_multimedia_script = f.read()
 
 class Migration(migrations.Migration):
 
@@ -44,11 +16,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(
-            create_multimedia_table,
-            delete_multimedia_table,
-        ),
-        migrations.RunSQL(
-            create_book_table,
-            delete_book_table,
+            create_multimedia_script,
+            drop_multimedia_script,
         ),
     ]
---
+++ schema/create_multimedia.sql
@@ -0,0 +1,21 @@
+CREATE TABLE multimedia (
+    id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL NULL,
+    created_at DATETIME NOT NULL,
+    modified_at DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE book (
+    id INT NOT NULL,
+    multimedia_id INT NOT NULL,
+    isbn13 CHAR(13) NULL,
+    isbn10 CHAR(10) NULL,
+    published_on DATE NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_book_multimedia
+        FOREIGN KEY (multimedia_id)
+        REFERENCES multimedia (id)
+);
