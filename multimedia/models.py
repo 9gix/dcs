7b2628af6639ceda77417ab7dbc527d496f9d830
Eugene <yeo.eugene.oey@gmail.com>
2015-03-13 01:41:23 +0800

    Simple Book Presentation

--- multimedia/models.py
+++ multimedia/models.py
@@ -1,6 +1,36 @@
 from django.db import connection
 from django.db import models
+from django.core.urlresolvers import reverse_lazy
 
+dummy_book = {
+    'isbn13': 9871234567890,
+    'isbn10': 1234567890,
+    'title': 'Hello World',
+    'url': reverse_lazy('multimedia:book_detail', kwargs={'isbn13': 9871234567890,}),
+    'description': '''
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Cras suscipit arcu sapien. Ut aliquet nisi est. 
+    Duis vulputate erat arcu.
+
+    Etiam vel ipsum quis est iaculis vehicula. 
+    Fusce faucibus ipsum nec ligula tempor ultrices.
+    Vestibulum tristique vitae risus lobortis volutpat.
+    ''',
+    'authors': [
+        {
+            'name': 'Foo',
+            'url': '/book-authors/foo/',
+        },
+        {
+            'name': 'Bar',
+            'url': '/book-authors/bar/',
+        },
+    ],
+    'publisher': {
+        'name': 'Exclusive Gix',
+        'url': '/book-publisher/exclusive-gix',
+    },
+}
 
 class Multimedia(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -17,6 +47,13 @@ class Multimedia(models.Model):
         db_table = 'multimedia'
 
 
+class BookManager(models.Manager):
+    def all(self):
+        return [dummy_book, dummy_book, dummy_book]
+
+    def get(self, *args, **kwargs):
+        return dummy_book
+
 class Book(Multimedia):
     multimedia = models.OneToOneField('Multimedia', parent_link=True)
     isbn13 = models.CharField(max_length=13)
@@ -24,9 +61,12 @@ class Book(Multimedia):
 
     published_on = models.DateField()
 
+    objects = BookManager()
+
     class Meta:
         managed = False
         db_table = 'book'
+
 
 
 class Music(Multimedia):
--- multimedia/urls.py
+++ multimedia/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('multimedia.views',
+    url(r'^books/$', 'book_list', name='book_list'),
+    url(r'^books/(?P<isbn13>\d{13})/', 'book_detail', name='book_detail'),
 )
