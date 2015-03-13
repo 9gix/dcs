8980d94d52c4a3244e3f191d669c2a246c811521
Nathan Azaria <nathanajah@yahoo.com>
2015-03-13 15:27:22 +0800

    Created migrations for the crew app

---
+++ crew/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+with open('schema/create_crew.sql') as f:
+    create_crew_script = f.read()
+
+with open('schema/drop_crew.sql') as f:
+    drop_crew_script = f.read()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            create_crew_script,
+            drop_crew_script
+        )
+    ]
---
+++ schema/drop_crew.sql
@@ -0,0 +1,4 @@
+DROP TABLE crew;
+DROP TABLE role;
+DROP TABLE organisation;
+DROP TABLE person;
