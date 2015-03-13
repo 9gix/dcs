c9c2c125eb2c25aac30b76c67eca684407c7345b
michael <mike.bdg@gmail.com>
2015-03-13 12:54:27 +0800

    Add migration script for album, music, and album_music

--- multimedia/migrations/0001_initial.py
+++ multimedia/migrations/0001_initial.py
@@ -9,6 +9,24 @@ with open('schema/create_multimedia.sql') as f:
 with open('schema/drop_multimedia.sql') as f:
     drop_multimedia_script = f.read()
 
+with open('schema/create_album.sql') as f:
+    create_album_script = f.read()
+
+with open('schema/drop_album.sql') as f:
+    drop_album_script = f.read()
+
+with open('schema/create_music.sql') as f:
+    create_music_script = f.read()
+
+with open('schema/drop_music.sql') as f:
+    drop_music_script = f.read()
+
+with open('schema/create_album_music.sql') as f:
+    create_album_music_script = f.read()
+
+with open('schema/drop_album_music.sql') as f:
+    drop_album_music_script = f.read()
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -19,4 +37,16 @@ class Migration(migrations.Migration):
             create_multimedia_script,
             drop_multimedia_script,
         ),
+        migrations.RunSQL(
+            create_album_script,
+            drop_album_script,
+        ),
+        migrations.RUNSQL(
+            create_music_script,
+            drop_music_script,
+        ),
+        migrations.RUNSQL(
+            create_album_music_script,
+            drop_album_music_script,
+        )
     ]
