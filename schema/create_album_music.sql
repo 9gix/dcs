824460006177963ca59376933fd4bb2192b50db9
michael <mike.bdg@gmail.com>
2015-03-13 15:21:55 +0800

    Combine SQL scripts into create_multimedia.sql

--- schema/create_album_music.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS album_music (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	album_id INT REFERENCES album(id),
-	music_id INT REFERENCES music(id)
-)
--- schema/create_music.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS music (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	multimedia_id INT REFERENCES multimedia(id),
-	duration INT
-)
