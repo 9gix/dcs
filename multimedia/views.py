b8c33ebccb807b3f20ceb4a1b6d16bf58811eb5f
Eugene <yeo.eugene.oey@gmail.com>
2015-04-03 22:42:46 +0800

    View Thumbnail on the template

--- multimedia/views.py
+++ multimedia/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, MultimediaImage
 
 
 def book_list(request):
     books = Book.objects.all()
+    book_ids = list(map(lambda book: book['id'], books))
+    #import ipdb; ipdb.set_trace()
+    multimedia_images = MultimediaImage.objects.filter(multimedia_id__in=book_ids)
+    for book in books:
+        multimedia_image = multimedia_images.filter(multimedia_id=book['id']).first()
+        if multimedia_image:
+            book['thumbnail'] = multimedia_image.thumbnail.url
+        else:
+            book['thumbnail'] = ''
     return render(request, 'multimedia/book_list.html', {'multimedia': books, 'multimedia_type': 'Book'})
 
 
