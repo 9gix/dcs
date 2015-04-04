f0d05b5ae09c9e35f189f7024726d4c3d17f9001
Eugene <yeo.eugene.oey@gmail.com>
2015-04-05 01:59:42 +0800

    Limit Options of Content Type

--- transaction/models.py
+++ transaction/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Sum
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
@@ -16,21 +17,29 @@ class Cart(models.Model):
         db_table = 'cart'
 
     def __str__(self):
-        import ipdb; ipdb.set_trace()
-        return self.buyer.username
+        shopping_list = "Total Quantity: {}".format(self.cartitem_set.aggregate(Sum('quantity'))['quantity__sum'])
+        return "{} - ({})".format(self.buyer.username, shopping_list)
 
 class CartItem(models.Model):
+
+    multimedia = (
+        models.Q(app_label='multimedia', model = 'Book') |
+        models.Q(app_label='multimedia', model = 'Movie') |
+        models.Q(app_label='multimedia', model = 'Application') |
+        models.Q(app_label='multimedia', model = 'Music') |
+        models.Q(app_label='multimedia', model = 'Album'))
+
     cart = models.ForeignKey('Cart')
     quantity = models.IntegerField()
-    content_type = models.ForeignKey(ContentType)
+    content_type = models.ForeignKey(ContentType, limit_choices_to=multimedia)
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey()
 
     class Meta:
         db_table = 'cart_item'
 
-    #def __str__(self):
-    #    return self.content_object
+    def __str__(self):
+        return str(self.content_object)
 
 class Transaction(models.Model):
     id = models.AutoField(primary_key=True)
@@ -41,6 +50,9 @@ class Transaction(models.Model):
     class Meta:
         managed = False
         db_table = 'transaction'
+
+    def __str__(self):
+        return self.cart
 
 
 
