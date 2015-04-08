0789994f5a71b8752e2d2f38916bf5e777417cf0
michael <mike.bdg@gmail.com>
2015-04-08 21:15:16 +0800

    Implement add to cart

--- transaction/urls.py
+++ transaction/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('transaction.views',
-	url(r'^purchase/', 'cart_purchase', name='cart_purchase'),
-	url(r'^delete/', 'cart_item_delete', name='cart_item_delete'),
-	url(r'^$', 'cart_item_list', name='cart_item_list'),
+    url(r'^purchase/', 'cart_purchase', name='cart_purchase'),
+    url(r'^delete/', 'cart_item_delete', name='cart_item_delete'),
+    url(r'^$', 'cart_item_list', name='cart_item_list'),
+    url(r'^(?P<multimedia_type>\w+)/(?P<multimedia_id>\d+)', 'cart_item_add', name='cart_item_add'),
 )
